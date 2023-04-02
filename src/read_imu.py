0f30cf7fdc79ca4e6a675e324d787285c703ea7e
Andrew Barlow <andrew.barlow@uwaterloo.ca>
2023-04-01 20:40:55 -0400

    debug read_imu.py

--- src/read_imu.py
+++ src/read_imu.py
@@ -5,18 +5,18 @@ import os
 def read_imu(dataset_date):
     filepath = f"dataset/{dataset_date}/ms25.csv"
     ms25 = np.loadtxt(filepath, delimiter = ",")
-    accel_x_OG = np.array(ms25[:, 4])
-    accel_y_OG = np.array(ms25[:, 5])
-    rot_h_OG   = np.array(ms25[:, 9])
-    t          = np.array(ms25[:, 0])
+    accel_x_OG = ms25[:, 4]
+    accel_y_OG = ms25[:, 5]
+    rot_h_OG   = ms25[:, 9]
+    t          = ms25[:, 0]
     
     # Relative timestamps
     t = t-t[0]
+    t = t/1000000
 
     # have the following format:
-    # timestamp | ax_robot | ay_robot | psi_dot
-    imu_data = np.concatenate((t, accel_x_OG, accel_y_OG, rot_h_OG), axis=1)
-
-
-
+    # timestamp | ax_robot | ay_robot | psi_dot    
+    imu_data = np.array([])
+    imu_data = np.vstack((t, accel_x_OG, accel_y_OG, rot_h_OG)).T
+    return imu_data
 
